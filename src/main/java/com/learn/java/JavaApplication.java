e5a1f7db4534e8d2bff73622c79a8e6065d63d7d
miao <asdfghjkl234567890>
2020-11-11 19:24:50 +0800

    spring boot  版本问题

--- src/main/java/com/learn/java/JavaApplication.java
+++ src/main/java/com/learn/java/JavaApplication.java
@@ -1,24 +1,34 @@
 package com.learn.java;
 
 import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
+import com.spring4all.swagger.EnableSwagger2Doc;
 import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@MapperScan("com.learn.java.*")
-@SpringBootApplication(exclude = DruidDataSourceAutoConfigure.class)
 @Slf4j
+@MapperScan("com.learn.java.mapper")
+@SpringBootApplication(exclude = DruidDataSourceAutoConfigure.class)
+@EnableSwagger2Doc
+@EnableScheduling
+@EnableConfigurationProperties
+
 public class JavaApplication {
 
     public static void main(String[] args) {
+        ConfigurableApplicationContext context = new SpringApplicationBuilder()
+                .sources(JavaApplication.class)
+                .main(JavaApplication.class)
+                .run(args);
+        context.registerShutdownHook();
 
-        SpringApplication.run(JavaApplication.class, args);
         log.info("==================================");
-        log.info("JavaApplication Server startup");
+        log.info("xinFinance Server startup");
         log.info("==================================");
-
     }
 
 }
